3b9b2bd7806e54f611a66057838cf87353ac545e
MelvinDettinger <94497044+MelvinDettinger@users.noreply.github.com>
2023-01-30 16:24:53 -0800

    Adding robot container code

--- src/main/java/frc/robot/commands/MoveWinch.java
+++ src/main/java/frc/robot/commands/MoveWinch.java
@@ -13,7 +13,7 @@
 package frc.robot.commands;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.CommandBase;
-import java.util.function.DoubleSupplier;
+
 
 import frc.robot.Robot;
 
